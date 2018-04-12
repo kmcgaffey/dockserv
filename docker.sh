1e97cc2bc197d0d89194568385ed663ad57ec85d
kmcgaffey <kenneth.mcgaffey@gmail.com>
2018-04-12 23:11:12 +0000

    beginning of docker install

---
+++ docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt-get update
+apt-get install \
+apt-transport-https \
+ca-certificates \ 
+curl \ 
+apt-get install software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+if [[ $(apt-key fingerprint 0EBFCD88 | grep 'Key fingerprint' ) != 'Key fingerprint = 9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88' ]]; then
+	echo Docker GPG key mismatch
+	exit
+fi
+
+ 
