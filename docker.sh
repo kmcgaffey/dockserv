ded677b9fc912e7491567cc99caf77550e6c4381
kmcgaffey <kenneth.mcgaffey@gmail.com>
2018-04-12 23:39:26 +0000

    docker environment complete

--- docker.sh
+++ docker.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 apt-get update
-apt-get install \
-apt-transport-https \
-ca-certificates \ 
-curl \ 
-apt-get install software-properties-common
+	apt-get install \
+	apt-transport-https \
+	ca-certificates \ 
+	curl \ 
+	apt-get install software-properties-common \
+	sudo add-apt-repository \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -14,4 +15,8 @@ if [[ $(apt-key fingerprint 0EBFCD88 | grep 'Key fingerprint' ) != 'Key fingerpr
 	exit
 fi
 
- 
+sudo add-apt-repository \
+	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) \
+	stable"
+apt-get install docker-ce 
