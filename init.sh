b3b3265e1795a7d57161217c69af2893e61606ca
Kenneth McGaffey <kenneth.mcgaffey@gmail.com>
2018-04-13 13:40:01 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,14 +6,23 @@ apt-get install apt-transport-https ca-certificates curl software-properties-com
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 if [[ $(apt-key fingerprint 0EBFCD88 | grep 'Key fingerprint' ) != 'Key fingerprint = 9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88' ]]; then
-	echo Docker GPG key mismatch
-	exit
+        echo Docker GPG key mismatch
+        exit
 fi
 
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-   
+
 apt-get update
 apt-get install docker-ce
+
+yes "" | ssh-keygen -t rsa -b 4096 -N "" -f git.key
+
+docker run -d \
+-p 80:80 -p 443:443 \
+-v $EXT_DIR:/etc/apache2/external/ \
+marvambass/apache2-ssl-secure
+
+git clone admin@localhost:/etc/git/admin.git
