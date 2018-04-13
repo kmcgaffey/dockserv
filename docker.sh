92c82de8730dcf306c61df199602aee9aa1ffcbb
Kenneth McGaffey <kenneth.mcgaffey@gmail.com>
2018-04-13 08:53:13 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,7 +5,7 @@ apt-get update
 	apt-transport-https \
 	ca-certificates \ 
 	curl \ 
-	software-properties-common \
+	software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
